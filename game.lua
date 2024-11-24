180b67832e95d1b2a233aa050b81a04edc924f3d
Liam Clinton <lacmuskoka@icloud.com>
2024-11-24 16:54:20 -0500

    improving SetupGame()

--- game.lua
+++ game.lua
@@ -9,7 +9,7 @@ require("utility.lua")
 -- setting up a new game
 function SetupGame()
     -- getting initial parameters
-    local liveShells, blankShells, maxBolts, opponentCount
+    local liveShells, blankShells, mBolts, opponentCount
 
     io.write("How many live (Red/White) shells? [0 to 10] >> ")
     liveShells = GetInput(0,1,2,3,4,5,6,7,8,9,10)
@@ -17,9 +17,19 @@ function SetupGame()
     io.write("How many blank (Grey/Black) shells? [0 to 10] >> ")
     blankShells = GetInput(0,1,2,3,4,5,6,7,8,9,10)
 
-    io.write("How many lives (Bolts) do you start with? [1 to 6] >> ")
-    maxBolts = GetInput(1,2,3,4,5,6)
+    io.write("How many bolts (Lives) are you starting with? [1 to 6] >> ")
+    mBolts = GetInput(1,2,3,4,5,6) -- maximum health for the round
 
     io.write("How many opponents? [1 to 3] >> ")
     opponentCount = GetInput(1,2,3)
+
+    Game = {
+        liveLeft = liveShells,
+        blankLeft = blankShells,
+        totalLeft = function() return Game.liveLeft + Game.blankLeft end, -- avoids getting/setting an inaccurate value
+        maxBolts = mBolts,
+        players = {} -- will fill later
+    }
+
+    -- creating the amount of players that we need
 end
