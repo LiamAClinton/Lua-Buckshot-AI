c27d69049a9a1343e7ca3fef95da5279a3661b8a
Liam Clinton <lacmuskoka@icloud.com>
2024-11-30 18:08:42 -0500

    finish SetupGame()

--- game.lua
+++ game.lua
@@ -20,28 +20,26 @@ function SetupGame()
     }
 
     io.write("How many opponents? [1 to 3] >> ")
-    Game.playerCount = GetInput(1,2,3) + 1; -- the +1 is accounting for the AI (AKA this program)
-
+    Game.playerCount = GetInput({1,2,3}) + 1; -- the +1 is accounting for the AI (AKA this program)
+    
+    io.write("How many bolts (Lives) are you starting with? [1 to 6] >> ")
+    Game.maxBolts = GetInput({1,2,3,4,5,6}) -- maximum health for the round
+    
     -- adding the players to account for (AI + opponentCount)
     for i = 1, Game.playerCount, 1 do
-        Game.Players[i] = {
+        Game.players.i = {
             currBolts = Game.maxBolts,
             nickname = nil,
             items = {}
         }
         -- allowing the player to set a nickname to help with their comprehension
-        io.write("What nickname would you like to give opponent " .. i .. "? >> ")
-        Game.players[i].nickname = io.read() -- only needed in this loop
+        io.write("What nickname would you like to give player " .. i .. "? >> ")
+        Game.players.i.nickname = io.read() -- only needed in this loop
     end
-    
-    io.write("How many bolts (Lives) are you starting with? [1 to 6] >> ")
-    Game.maxBolts = GetInput(1,2,3,4,5,6) -- maximum health for the round
-    
+
     io.write("How many live (Red/White) shells? [0 to 10] >> ")
-    Game.liveLeft = GetInput(0,1,2,3,4,5,6,7,8,9,10)
+    Game.liveLeft = GetInput({0,1,2,3,4,5,6,7,8,9,10})
 
     io.write("How many blank (Grey/Black) shells? [0 to 10] >> ")
-    Game.blankLeft = GetInput(0,1,2,3,4,5,6,7,8,9,10)
-
-    print(Game)
+    Game.blankLeft = GetInput({0,1,2,3,4,5,6,7,8,9,10})
 end
